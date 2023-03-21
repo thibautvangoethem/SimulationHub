be465ad37d62bcf9212d62ed878ddbb2a7502eac
thibautvangoethem <thibautvangoethem2@gmail.com>
2023-03-22 00:03:56 +0100

    Flattened all vectors to allow usage of _mm256_i32gather_ps

--- simulations/fluidsim/FluidSim.cpp
+++ simulations/fluidsim/FluidSim.cpp
@@ -7,24 +7,12 @@ using namespace SIM;
 
 FluidSim::FluidSim(std::unique_ptr<SIM::SimulationSettings> settings) : Simulation(std::move(settings)) {
 	m_size = this->m_settings->getSize();
-	m_s = grid(
-		m_size,
-		std::vector<float>(m_size));
-	m_density = grid(
-		m_size,
-		std::vector<float>(m_size));
-	m_velX = grid(
-		m_size,
-		std::vector<float>(m_size));
-	m_velY = grid(
-		m_size,
-		std::vector<float>(m_size));
-	m_oldVelX = grid(
-		m_size,
-		std::vector<float>(m_size));
-	m_oldVelY = grid(
-		m_size,
-		std::vector<float>(m_size));
+	m_s = std::vector<float>(m_size * m_size, 0);
+	m_density = std::vector<float>(m_size * m_size, 0);
+	m_velX = std::vector<float>(m_size * m_size, 0);
+	m_velY = std::vector<float>(m_size * m_size, 0);
+	m_oldVelX = std::vector<float>(m_size * m_size, 0);
+	m_oldVelY = std::vector<float>(m_size * m_size, 0);
 
 	m_currentState = std::vector<std::vector<SIM::colour> >(
 		m_size,
@@ -60,36 +48,36 @@ std::vector<std::vector<SIM::colour>>& FluidSim::getCurrentState() {
 		for (int j = 0; j < m_size; ++j)
 		{
 
-			int val = m_s[i][j]* 52;
+			int val = m_s[(i)*m_size + (j)]* 52;
 			if (val > 255)val = 255;
 			if (val < 0)val = 0;
 			auto valCasted = (uint8_t)(val);
 			//TODO asses the performance impact of this calculation
-			float combinedSpeedVal =1-sqrt(m_velY[i][j]* m_velY[i][j] + m_velX[i][j]* m_velX[i][j]);
+			float combinedSpeedVal =1-sqrt(m_velY[(i)*m_size + (j)]* m_velY[(i)*m_size + (j)] + m_velX[(i)*m_size + (j)]* m_velX[(i)*m_size + (j)]);
 			
-			m_currentState[j][i] = colour{ valCasted,(uint8_t)(valCasted* combinedSpeedVal),(uint8_t)(valCasted* combinedSpeedVal) };
+			m_currentState[(j)][(i)] = colour{ valCasted,(uint8_t)(valCasted* combinedSpeedVal),(uint8_t)(valCasted* combinedSpeedVal) };
 		}
 	}
 	if(m_storedClick)
 	{
-		m_currentState[m_storedClick->second][m_storedClick->first]= colour{ 0,255,0 };
+		m_currentState[(m_storedClick->second)][(m_storedClick->first)]= colour{ 0,255,0 };
 	}
 	return m_currentState;
 }
 
 void FluidSim::addDensity(const int x,const int y,const float amount) {
-	m_density[x][y] += amount;
+	m_density[(x)*m_size + (y)] += amount;
 }
 
 void FluidSim::addVelocity(const int x,const int y,const float amount_x,const float amount_y) {
-	m_velX[x][y] += amount_x;
-	m_velY[x][y] += amount_y;
+	m_velX[(x)*m_size + (y)] += amount_x;
+	m_velY[(x)*m_size + (y)] += amount_y;
 }
 
 void FluidSim::applySources() {
 	for (auto& flow : m_flows) {
-		m_velX[flow.location.first][flow.location.second] = flow.velocity.first;
-		m_velY[flow.location.first][flow.location.second] = flow.velocity.second;
+		m_velX[(flow.location.first)*m_size + (flow.location.second)] = flow.velocity.first;
+		m_velY[(flow.location.first)*m_size + (flow.location.second)] = flow.velocity.second;
 	}
 
 	for (auto& source : m_paintSources) {
@@ -99,21 +87,21 @@ void FluidSim::applySources() {
 }
 
 
-void FluidSim::setBoundary(const int b, std::vector<std::vector<float>>& array) {
+void FluidSim::setBoundary(const int b, std::vector<float>& array) {
 	for (unsigned int i = 1; i < m_size - 1; i++)
 	{
-		array[i][0] = (b == 2 ? -array[i][1] : array[i][1]);
-		array[i][m_size - 1] = (b == 2 ? -array[i][m_size - 2] : array[i][m_size - 2]);
+		array[(i)*m_size + (0)] = (b == 2 ? -array[(i)*m_size + (1)] : array[(i)*m_size + (1)]);
+		array[(i)*m_size + (m_size - 1)] = (b == 2 ? -array[(i)*m_size + (m_size - 2)] : array[(i)*m_size + (m_size - 2)]);
 	}
 	for (unsigned int i = 1; i < m_size - 1; i++)
 	{
-		array[0][i] = (b == 1 ? -array[1][i] : array[1][i]);
-		array[m_size - 1][i] = (b == 1 ? -array[m_size - 2][i] : array[m_size - 2][i]);
+		array[(0)*m_size + (i)] = (b == 1 ? -array[(1)*m_size + (i)] : array[(1)*m_size + (i)]);
+		array[(m_size - 1)*m_size + (i)] = (b == 1 ? -array[(m_size - 2)*m_size + (i)] : array[(m_size - 2)*m_size + (i)]);
 	}
-	array[0][0] = 0.5 * array[1][0] + array[0][1];
-	array[0][m_size - 1] = 0.5 * (array[1][m_size - 1] + array[0][m_size - 2]);
-	array[m_size - 1][0] = 0.5 * (array[m_size - 2][0] + array[m_size - 1][1]);
-	array[m_size - 1][m_size - 1] = 0.5 * (array[m_size - 2][m_size - 1] + array[m_size - 1][m_size - 2]);
+	array[(0)*m_size + (0)] = 0.5 * array[(1)*m_size + (0)] + array[(0)*m_size + (1)];
+	array[(0)*m_size + ((m_size - 1))] = 0.5 * (array[((1))*m_size + ((m_size - 1))] + array[(0)*m_size + (m_size - 2)]);
+	array[(m_size - 1)*m_size + (0)] = 0.5 * (array[(m_size - 2)*m_size + (0)] + array[(m_size - 1)*m_size + (1)]);
+	array[(m_size - 1)*m_size + (m_size - 1)] = 0.5 * (array[(m_size - 2)*m_size + (m_size - 1)] + array[(m_size - 1)*m_size + (m_size - 2)]);
 }
 
 /*
@@ -125,11 +113,11 @@ void FluidSim::setBoundary(const int b, std::vector<std::vector<float>>& array) 
 		for (unsigned int j = 1; j < m_size - 1; j += 8) // Process 8 elements at a time
 		{
 			// Load 8 elements from the arrays into 256-bit vectors
-			__m256 prevArray_vec = _mm256_loadu_ps(&prevArray[i][j]);
-			__m256 array_up_vec = _mm256_loadu_ps(&array[i - 1][j]);
-			__m256 array_down_vec = _mm256_loadu_ps(&array[i + 1][j]);
-			__m256 array_left_vec = _mm256_loadu_ps(&array[i][j - 1]);
-			__m256 array_right_vec = _mm256_loadu_ps(&array[i][j + 1]);
+			__m256 prevArray_vec = _mm256_loadu_ps(&prevArray[(i)*m_size + (j)]);
+			__m256 array_up_vec = _mm256_loadu_ps(&array[(i - 1)*m_size + (j)]);
+			__m256 array_down_vec = _mm256_loadu_ps(&array[(i + 1)*m_size + (j)]);
+			__m256 array_left_vec = _mm256_loadu_ps(&array[(i)*m_size + (j - 1)]);
+			__m256 array_right_vec = _mm256_loadu_ps(&array[(i)*m_size + (j + 1)]);
 			
 			// Add the 5 vectors together and multiply by 'a_vec'
 			__m256 sum_vec = _mm256_add_ps(_mm256_add_ps(_mm256_add_ps(_mm256_add_ps(prevArray_vec, array_up_vec), array_down_vec), array_left_vec), array_right_vec);
@@ -137,12 +125,12 @@ void FluidSim::setBoundary(const int b, std::vector<std::vector<float>>& array) 
 			
 			// Multiply by 'cRecip_vec' and store the result back into the array
 			__m256 result_vec = _mm256_mul_ps(cRecip_vec, prod_vec);
-			_mm256_storeu_ps(&array[i][j], result_vec);
+			_mm256_storeu_ps(&array[(i)*m_size + (j)], result_vec);
 		}
 	}
  */
 #ifdef AVX
-void FluidSim::linSolveAvx(const int b, std::vector<std::vector<float>>& array, const std::vector<std::vector<float>>& prevArray, const float a, const float c)
+void FluidSim::linSolveAvx(const int b, std::vector<float>& array, const std::vector<float>& prevArray, const float a, const float c)
 {
 	const float cRecip = 1.0 / c;
 	const auto a_vec = _mm256_set1_ps(a); 
@@ -151,23 +139,23 @@ void FluidSim::linSolveAvx(const int b, std::vector<std::vector<float>>& array, 
 	{
 		for (unsigned int j = 1; j < m_size - 1; j += 8) // Process 8 elements at a time due to avx2 taking 8 floats per operation
 		{
-			const auto prevArray_vec = _mm256_loadu_ps(&prevArray[i][j]);
-			const auto array_up_vec = _mm256_loadu_ps(&array[i - 1][j]);
-			const auto array_down_vec = _mm256_loadu_ps(&array[i + 1][j]);
-			const auto array_left_vec = _mm256_loadu_ps(&array[i][j - 1]);
-			const auto array_right_vec = _mm256_loadu_ps(&array[i][j + 1]);
+			const auto prevArray_vec = _mm256_loadu_ps(&prevArray[(i)*m_size + (j)]);
+			const auto array_up_vec = _mm256_loadu_ps(&array[(i - 1)*m_size + (j)]);
+			const auto array_down_vec = _mm256_loadu_ps(&array[(i + 1)*m_size + (j)]);
+			const auto array_left_vec = _mm256_loadu_ps(&array[(i)*m_size + (j - 1)]);
+			const auto array_right_vec = _mm256_loadu_ps(&array[(i)*m_size + (j + 1)]);
 
 			const auto sum_vec = _mm256_add_ps(_mm256_add_ps(_mm256_add_ps(array_up_vec, array_down_vec), array_left_vec), array_right_vec);
 			const auto prod_vec = _mm256_mul_ps(a_vec, sum_vec);
 			const auto sum2_vec = _mm256_add_ps(prevArray_vec, prod_vec);
 
 			const auto result_vec = _mm256_mul_ps(cRecip_vec, sum2_vec);
-			_mm256_storeu_ps(&array[i][j], result_vec);
+			_mm256_storeu_ps(&array[(i)*m_size + (j)], result_vec);
 		}
 	}
 }
 
-void FluidSim::advectAvx(const int b, std::vector<std::vector<float>>& d, const std::vector<std::vector<float>>& prevD, const std::vector<std::vector<float>>& velocityX, const std::vector<std::vector<float>>& velocityY, const float dt)
+void FluidSim::advectAvx(const int b, std::vector<float>& d, const std::vector<float>& prevD, const std::vector<float>& velocityX, const std::vector<float>& velocityY, const float dt)
 {
 	std::vector<int> resulti0i(8);
 	std::vector<int> resulti1i(8);
@@ -182,6 +170,7 @@ void FluidSim::advectAvx(const int b, std::vector<std::vector<float>>& d, const 
 	const auto dty_vec = _mm256_set1_ps(dt * (m_size - 2));
 
 	const auto one_vec = _mm256_set1_epi32(1);
+	const auto mSize_vec = _mm256_set1_epi32(m_size);
 	const auto one_vecps = _mm256_set1_ps(1.0f);
 	const auto half_vecps = _mm256_set1_ps(0.5f);
 
@@ -197,8 +186,8 @@ void FluidSim::advectAvx(const int b, std::vector<std::vector<float>>& d, const 
 	{
 		for (unsigned int j = 1; j < m_size - 1; j+=8) // Process 8 elements at a time due to avx2 taking 8 floats per operation
 		{
-			auto velx_vec = _mm256_loadu_ps(&velocityX[i][j]);
-			auto vely_vec = _mm256_loadu_ps(&velocityY[i][j]);
+			auto velx_vec = _mm256_loadu_ps(&velocityX[(i)*m_size + (j)]);
+			auto vely_vec = _mm256_loadu_ps(&velocityY[(i)*m_size + (j)]);
 			// Compute the position at which to interpolate the density field
 			auto tmp1_vec = _mm256_mul_ps(dtx_vec, velx_vec);
 			auto tmp2_vec = _mm256_mul_ps(dty_vec, vely_vec);
@@ -241,27 +230,23 @@ void FluidSim::advectAvx(const int b, std::vector<std::vector<float>>& d, const 
 
 			//Annoyingly have to go out of avx mode as I used nested vectors which cant be accessed in a flattened way  for use with gather avx function
 			//TODO change this so _mm256_i32gather_epi32 can be used
+
+			//no fnmadd for epi32 :(
+			auto prevDFirstIndex_vex= _mm256_add_epi32(_mm256_mullo_epi32(i0i_vec, mSize_vec), j0i_vec);
+			auto prevDSecondIndex_vex = _mm256_add_epi32(_mm256_mullo_epi32(i0i_vec, mSize_vec), j1i_vec);
+			auto prevDThirdIndex_vex = _mm256_add_epi32(_mm256_mullo_epi32(i1i_vec, mSize_vec), j0i_vec);
+			auto prevDFourthIndex_vex = _mm256_add_epi32(_mm256_mullo_epi32(i1i_vec, mSize_vec), j1i_vec);
+
+			auto prevDFirst_vec= _mm256_i32gather_ps(&prevD[0], prevDFirstIndex_vex, 4);
+			auto prevDSecond_vec = _mm256_i32gather_ps(&prevD[0], prevDSecondIndex_vex, 4);
+			auto prevDThird_vec = _mm256_i32gather_ps(&prevD[0], prevDThirdIndex_vex, 4);
+			auto prevDFourth_vec = _mm256_i32gather_ps(&prevD[0], prevDFourthIndex_vex, 4);
 			
-			_mm256_storeu_epi32(&resulti0i[0], i0i_vec);
-			_mm256_storeu_epi32(&resulti1i[0], i1i_vec);
-			_mm256_storeu_epi32(&resultj0i[0], j0i_vec);
-			_mm256_storeu_epi32(&resultj1i[0], j1i_vec);
-			for(int prevDIterator=0; prevDIterator <8; prevDIterator++)
-			{
-				prevDFirst[prevDIterator] = prevD[resulti0i[prevDIterator]][resultj0i[prevDIterator]];
-				prevDSecond[prevDIterator] = prevD[resulti0i[prevDIterator]][resultj1i[prevDIterator]];
-				prevDThird[prevDIterator] = prevD[resulti1i[prevDIterator]][resultj0i[prevDIterator]];
-				prevDFourth[prevDIterator] = prevD[resulti1i[prevDIterator]][resultj1i[prevDIterator]];
-			}
-			auto prevDFirst_vec = _mm256_loadu_ps(&prevDFirst[0]);
-			auto prevDSecond_vec = _mm256_loadu_ps(&prevDSecond[0]);
-			auto prevDThird_vec = _mm256_loadu_ps(&prevDThird[0]);
-			auto prevDFourth_vec = _mm256_loadu_ps(&prevDFourth[0]);
 
 			auto part1 = _mm256_fmadd_ps(t0_vec, prevDFirst_vec, _mm256_mul_ps(t1_vec, prevDSecond_vec));
 			auto part2 = _mm256_fmadd_ps(t0_vec, prevDThird_vec, _mm256_mul_ps(t1_vec, prevDFourth_vec));
 			auto result_vec = _mm256_fmadd_ps(s0_vec, part1, _mm256_mul_ps(s1_vec, part2));
-			_mm256_storeu_ps(&d[i][j], result_vec);
+			_mm256_storeu_ps(&d[(i)*m_size + (j)], result_vec);
 			
 		}
 	}
@@ -269,7 +254,7 @@ void FluidSim::advectAvx(const int b, std::vector<std::vector<float>>& d, const 
 
 #endif
 
-void FluidSim::linSolve(const int b, std::vector<std::vector<float>>& array, const std::vector<std::vector<float>>& prevArray,const float a,const float c) {
+void FluidSim::linSolve(const int b, std::vector<float>& array, const std::vector<float>& prevArray,const float a,const float c) {
 	
 #ifdef AVX
 	linSolveAvx(b, array, prevArray, a, c);
@@ -280,25 +265,25 @@ void FluidSim::linSolve(const int b, std::vector<std::vector<float>>& array, con
 	{
 		for (unsigned int j = 1; j < m_size - 1; j++)
 		{
-			array[i][j] = (prevArray[i][j] + a * (array[i + 1][j] + array[i - 1][j] + array[i][j + 1] + array[i][j - 1])) * cRecip;
+			array[(i)*m_size + (j)] = (prevArray[(i)*m_size + (j)] + a * (array[(i + 1)*m_size + (j)] + array[(i - 1)*m_size + (j)] + array[(i)*m_size + (j + 1)] + array[(i)*m_size + (j - 1)])) * cRecip;
 		}
 	}
 #endif
 
 	this->setBoundary(b, array);
 }
-void FluidSim::diffuse(const int b, std::vector<std::vector<float>>& array, const std::vector<std::vector<float>>& prevArray, const float diff, const float dt) {
+void FluidSim::diffuse(const int b, std::vector<float>& array, const std::vector<float>& prevArray, const float diff, const float dt) {
 	float a = dt * diff * (m_size - 2) * (m_size - 2);
 	linSolve(b, array, prevArray, a, 1 + 6 * a);
 
 }
-void FluidSim::project(std::vector<std::vector<float>>& velocityX, std::vector<std::vector<float>>& velocityY, std::vector<std::vector<float>>& clearVector, std::vector<std::vector<float>>& targetVectory) {
+void FluidSim::project(std::vector<float>& velocityX, std::vector<float>& velocityY, std::vector<float>& clearVector, std::vector<float>& targetVectory) {
 	for (unsigned int j = 1; j < m_size - 1; j++)
 	{
 		for (unsigned int i = 1; i < m_size - 1; i++)
 		{
-			targetVectory[j][i] = - 0.5 * (velocityX[j + 1][i] - velocityX[j - 1][i] + velocityY[j][i + 1] - velocityY[j][i - 1]) / m_size;
-			clearVector[j][i] = 0;
+			targetVectory[(j)*m_size + (i)] = - 0.5 * (velocityX[(j + 1)*m_size + (i)] - velocityX[(j - 1)*m_size + (i)] + velocityY[(j)*m_size + (i + 1)] - velocityY[(j)*m_size + (i - 1)]) / m_size;
+			clearVector[(j)*m_size + (i)] = 0;
 		}
 	}
 	this->setBoundary(0, targetVectory);
@@ -309,8 +294,8 @@ void FluidSim::project(std::vector<std::vector<float>>& velocityX, std::vector<s
 	{
 		for (unsigned int j = 1; j < m_size - 1; j++)
 		{
-			velocityX[i][j] = velocityX[i][j] - 0.5 * (clearVector[i + 1][j] - clearVector[i - 1][j]) * m_size;
-			velocityY[i][j] = velocityY[i][j] - 0.5 * (clearVector[i][j + 1] - clearVector[i][j - 1]) * m_size;
+			velocityX[(i)*m_size + (j)] = velocityX[(i)*m_size + (j)] - 0.5 * (clearVector[(i + 1)*m_size + (j)] - clearVector[(i - 1)*m_size + (j)]) * m_size;
+			velocityY[(i)*m_size + (j)] = velocityY[(i)*m_size + (j)] - 0.5 * (clearVector[(i)*m_size + (j + 1)] - clearVector[(i)*m_size + (j - 1)]) * m_size;
 		}
 	}
 	__m256i mask = _mm256_setr_epi32(-20, -72, -48, -9, -100, 3, 5, 8);
@@ -321,7 +306,7 @@ void FluidSim::project(std::vector<std::vector<float>>& velocityX, std::vector<s
 // This function performs advection of a fluid property (represented by the density field 'd') 
 // using the velocity field (represented by 'velocityX' and 'velocityY') over a timestep 'dt'
 // for a given boundary 'b'. It uses the previous state of the density field ('prevD') as input.
-void FluidSim::advect(const int b, std::vector<std::vector<float>>& d, const std::vector<std::vector<float>>& prevD, const std::vector<std::vector<float>>& velocityX,const std::vector<std::vector<float>>& velocityY, const float dt) {
+void FluidSim::advect(const int b, std::vector<float>& d, const std::vector<float>& prevD, const std::vector<float>& velocityX,const std::vector<float>& velocityY, const float dt) {
 #ifdef AVX
 	advectAvx(b, d, prevD, velocityX,velocityY, dt);
 #else
@@ -343,8 +328,8 @@ void FluidSim::advect(const int b, std::vector<std::vector<float>>& d, const std
 		for (unsigned int j = 1; j < m_size - 1; j++)
 		{
 			// Compute the position at which to interpolate the density field
-			tmp1 = dtx * velocityX[i][j];
-			tmp2 = dty * velocityY[i][j];
+			tmp1 = dtx * velocityX[(i)*m_size + (j)];
+			tmp2 = dty * velocityY[(i)*m_size + (j)];
 			x = i - tmp1;
 			y = j - tmp2;
 
@@ -368,8 +353,8 @@ void FluidSim::advect(const int b, std::vector<std::vector<float>>& d, const std
 			int i1i = static_cast<int>(i1 + 0.5);
 			int j0i = static_cast<int>(j0 + 0.5);
 			int j1i = static_cast<int>(j1 + 0.5);
-			auto te= s0 * (t0 * prevD[i0i][j0i] + t1 * prevD[i0i][j1i]) + s1 * (t0 * prevD[i1i][j0i] + t1 * prevD[i1i][j1i]);
-			d[i][j] = te;
+			auto te= s0 * (t0 * prevD[(i0i)*m_size + (j0i)] + t1 * prevD[(i0i)*m_size + (j1i)]) + s1 * (t0 * prevD[(i1i)*m_size + (j0i)] + t1 * prevD[(i1i)*m_size + (j1i)]);
+			d[(i)*m_size + (j)] = te;
 		}
 	}
 #endif
--- simulations/fluidsim/FluidSim.h
+++ simulations/fluidsim/FluidSim.h
@@ -23,35 +23,35 @@ namespace SIM {
 		void addVelocity(const int x, const int y, const float amount_x, const float amount_y);
 		void applySources();
 
-		void setBoundary(const int b, std::vector<std::vector<float>>& array);
-		void linSolve(const int b, std::vector<std::vector<float>>& array, const std::vector<std::vector<float>>& prevArray, const float a, const float c);
-		void diffuse(const int b, std::vector<std::vector<float>>& array, const std::vector<std::vector<float>>& prevArray, const float diff, const float dt);
-		void project(std::vector<std::vector<float>>& velocityX, std::vector<std::vector<float>>& velocityY, std::vector<std::vector<float>>& clearVector, std::vector<std::vector<float>>& targetVectory);
-		void advect(const int b, std::vector<std::vector<float>>& d, const std::vector<std::vector<float>>& prevD, const std::vector<std::vector<float>>& velocityX, const std::vector<std::vector<float>>& velocityY, const float dt);
+		void setBoundary(const int b, std::vector<float>& array);
+		void linSolve(const int b, std::vector<float>& array, const std::vector<float>& prevArray, const float a, const float c);
+		void diffuse(const int b, std::vector<float>& array, const std::vector<float>& prevArray, const float diff, const float dt);
+		void project(std::vector<float>& velocityX, std::vector<float>& velocityY, std::vector<float>& clearVector, std::vector<float>& targetVectory);
+		void advect(const int b, std::vector<float>& d, const std::vector<float>& prevD, const std::vector<float>& velocityX, const std::vector<float>& velocityY, const float dt);
 
 
 		//Functions that use avx2 functions for acceleration
 #ifdef AVX
-		void linSolveAvx(const int b, std::vector<std::vector<float>>& array, const std::vector<std::vector<float>>& prevArray, const float a, const float c);
-		void advectAvx(const int b, std::vector<std::vector<float>>& d, const std::vector<std::vector<float>>& prevD, const std::vector<std::vector<float>>& velocityX, const std::vector<std::vector<float>>& velocityY, const float dt);
+		void linSolveAvx(const int b, std::vector<float>& array, const std::vector<float>& prevArray, const float a, const float c);
+		void advectAvx(const int b, std::vector<float>& d, const std::vector<float>& prevD, const std::vector<float>& velocityX, const std::vector<float>& velocityY, const float dt);
 #endif
 
 
-		typedef std::vector<std::vector<float>> grid;
+		
 
 		int m_size;
 
 		std::vector<Flow> m_flows = std::vector<Flow>();
 		std::vector<std::pair<int, int>> m_paintSources = std::vector<std::pair<int, int>>();
 
-		grid m_s;
-		grid m_density;
+		std::vector<float> m_s;
+		std::vector<float> m_density;
 
-		grid m_velX;
-		grid m_velY;
+		std::vector<float> m_velX;
+		std::vector<float> m_velY;
 
-		grid m_oldVelX;
-		grid m_oldVelY;
+		std::vector<float> m_oldVelX;
+		std::vector<float> m_oldVelY;
 
 		std::vector<std::vector<SIM::colour>> m_currentState;
 
